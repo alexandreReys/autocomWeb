60e49203c46067b1e065c1a63b56967b9b2db6bc
Alexandre Reys <anr.alexandre@gmail.com>
2020-01-06 14:21:31 -0300

    fix layoutNavbar not collapsing before click

--- src/components/layout/LayoutNavbar.vue
+++ src/components/layout/LayoutNavbar.vue
@@ -1,31 +1,34 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-      <router-link class="navbar-brand" :to="{ name: 'home' }">
-        AutoCom Web
-      </router-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse"
-      data-target="#navbarText" aria-controls="navbarText" aria-expanded="false"
-      aria-label="Toggle navigation" @click="toggleNavbar()"
+    <router-link class="navbar-brand" :to="{ name: 'home' }">AutoCom Web</router-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-target="#navbarText"
+      aria-controls="navbarText"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+      @click="toggleNavbar()"
     >
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText" v-bind:class="{ show: show }">
       <ul class="navbar-nav mr-auto font-size12">
-          <router-link class="nav-item" tag="li"
-            exact
-            :key="i"
-            exact-active-class="active"
-            :to="{ name: route.name }"
-            v-for="(route, i) in routes"
-          >
-            <a href="#" class="nav-link">{{ route.meta.title }}</a>
-          </router-link>
+        <router-link
+          class="nav-item"
+          tag="li"
+          exact
+          :key="i"
+          exact-active-class="active"
+          :to="{ name: route.name }"
+          v-for="(route, i) in routes"
+        >
+          <a href="#" class="nav-link" @click="volta()">{{ route.meta.title }}</a>
+        </router-link>
       </ul>
       <span class="navbar-text">
         <div v-if="user.name">
-          <a class="nav-link pointer font-size12" @click="logout()">
-            Olá, {{ user.name }} |  Sair
-          </a>
+          <a class="nav-link pointer font-size12" @click="logout()">Olá, {{ user.name }} | Sair</a>
         </div>
       </span>
     </div>
@@ -42,11 +45,11 @@ export default {
     }
   },
   computed: {
-    ...mapState('auth', ['user']),
+    ...mapState( 'auth', ['user'] ),
     routes () {
-      return this.$router.options.routes.filter(route => (
+      return this.$router.options.routes.filter( route => (
         route.meta && route.meta.showNavbar
-      ))
+      ) )
     }
   },
   methods: {
@@ -56,18 +59,21 @@ export default {
     async logout () {
       try {
         await this.ActionSignOut()
-        this.$router.push({ name: 'login' })
-      } catch (err) {
-        console.log(err)
-        alert(err.data ? err.data.message : 'Não foi possivel fazer logout')
+        this.$router.push( { name: 'login' } )
+      } catch ( err ) {
+        console.log( err )
+        alert( err.data ? err.data.message : 'Não foi possivel fazer logout' )
       }
     },
     toggleNavbar () {
       this.show = !this.show
+    },
+    volta () {
+      this.show = false
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
 </style>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,7 +4,8 @@
       <h2>Olá, {{ user.name }}</h2>
       <h4 class="font-yellow mt-3">
         <!-- <i class="material-icons md-48">laptop_chromebook</i> -->
-        Bem vindo ao <span class="font-bold"> AutoCom Web </span>
+        Bem vindo ao
+        <span class="font-bold">AutoCom Web</span>
       </h4>
       <h5 class="mt-2">Software para Lojas, Serviços e Alimentação</h5>
     </div>
@@ -19,11 +20,6 @@
         <i class="material-icons md-36">{{ route.meta.icon }}</i>
         <span class="mt-3">{{ route.meta.label }}</span>
       </router-link>
-
-      <a href="#" class="home-link">
-        <i class="material-icons md-36">info_outline</i>
-        <span class="mt-3">Sobre</span>
-      </a>
     </div>
   </div>
 </template>
@@ -34,11 +30,11 @@ import { mapState } from 'vuex'
 export default {
   name: 'Home',
   computed: {
-    ...mapState('auth', ['user']),
+    ...mapState( 'auth', ['user'] ),
     routes () {
-      return this.$router.options.routes.filter(route => (
+      return this.$router.options.routes.filter( route => (
         route.meta && route.meta.showNavbar && route.meta.label
-      ))
+      ) )
     }
   }
 }
@@ -66,27 +62,34 @@ export default {
   }
 
   .home-links {
-    width: 100%;
+    width: 170%;
     @include flex-center();
     .home-link {
       flex: 0 0 15%;
-      margin: 0 25px 50px;
+      margin: 0 10px 50px;
       padding: 30px 0;
-      transition: .4s;
+      transition: 0.4s;
       color: var(--white);
       @include flex-center(true);
       &:hover {
         transform: scale(1.1);
         text-decoration: none;
-        box-shadow: 0 10px 20px white
+        box-shadow: 0 10px 20px white;
         // box-shadow: 0 10px 20px rgba(0,0,0,0.5)
       }
     }
   }
-  .home-link:first-child { background-color: var(--blue) }
-  .home-link:nth-child(2) { background-color: var(--orange) }
-  .home-link:nth-child(3) { background-color: var(--green) }
-  .home-link:last-child { background-color: var(--red) }
-
+  .home-link:first-child {
+    background-color: var(--blue);
+  }
+  .home-link:nth-child(2) {
+    background-color: var(--orange);
+  }
+  .home-link:nth-child(3) {
+    background-color: var(--green);
+  }
+  .home-link:last-child {
+    background-color: var(--red);
+  }
 }
 </style>
