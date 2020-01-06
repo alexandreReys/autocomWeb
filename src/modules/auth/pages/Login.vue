2dc912f03bf5a53b8370f0fc8d4292bdf508ea6e
Alexandre Reys <anr.alexandre@gmail.com>
2020-01-06 11:46:39 -0300

    refact login screen for mobile screen

--- src/modules/auth/pages/Login.vue
+++ src/modules/auth/pages/Login.vue
@@ -3,7 +3,7 @@
     <form @submit.prevent="submit()">
       <div class="login-page">
         <div class="card mt-5">
-          <div class="card-header">Login</div>
+          <div class="card-header">AutocomWeb - Login</div>
           <div class="card-body">
             <div class="form-group">
               <input
@@ -12,7 +12,7 @@
                 v-model="form.email"
                 class="form-control"
                 placeholder="E-mail"
-              >
+              />
             </div>
             <div class="form-group">
               <input
@@ -21,11 +21,9 @@
                 v-model="form.password"
                 class="form-control"
                 placeholder="Senha"
-              >
+              />
             </div>
-            <button class="btn btn-primary d-w-100">
-              Entrar
-            </button>
+            <button class="btn btn-primary d-w-100">Entrar</button>
           </div>
         </div>
       </div>
@@ -37,21 +35,21 @@
 import { mapActions } from 'vuex'
 
 export default {
-  data: () => ({
+  data: () => ( {
     form: {
       email: '',
       password: ''
     }
-  }),
+  } ),
   methods: {
-    ...mapActions('auth', ['ActionDoLogin']),
+    ...mapActions( 'auth', ['ActionDoLogin'] ),
     async submit () {
       try {
-        await this.ActionDoLogin(this.form)
-        this.$router.push({ name: 'home' })
-      } catch (err) {
-        console.log(err)
-        alert(err.data ? err.data.message : 'Não foi possivel fazer login')
+        await this.ActionDoLogin( this.form )
+        this.$router.push( { name: 'home' } )
+      } catch ( err ) {
+        console.log( err )
+        alert( err.data ? err.data.message : 'Não foi possivel fazer login' )
       }
     }
   }
@@ -59,14 +57,25 @@ export default {
 </script>
 
 <style>
-  .login-page {
-    height: 300px;
-    display: flex;
-    /* align-items: center; */
-    justify-content: center;
-    color: darkblue;
-  }
+.login-page {
+  height: 300px;
+  display: flex;
+  /* align-items: center; */
+  justify-content: center;
+  color: darkblue;
+}
+
+.card {
+  width: 40%;
+}
+@media (max-width: 600px) {
   .card {
-    width: 30%;
+    width: 70%;
   }
+}
+@media (min-width: 601px) and (max-width: 900px) {
+  .card {
+    width: 50%;
+  }
+}
 </style>
